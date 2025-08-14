8dda3efd052cc83ed47e67aa50ca785e4e56f226
Ruslan Karimov <98585383+IamKarimovich@users.noreply.github.com>
2025-08-14 23:48:15 +0400

    Bean creation and context.xml have fullfilled

--- src/main/java/com/ruslandev/springbeancreation/car/beans/Application.java
+++ src/main/java/com/ruslandev/springbeancreation/car/beans/Application.java
@@ -1,4 +1,15 @@
 package com.ruslandev.springbeancreation.car.beans;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+
+        MyBean myBean = (MyBean) context.getBean("myBean");
+
+        System.out.println(myBean);
+
+    }
 }
--- src/main/java/com/ruslandev/springbeancreation/car/beans/MyBean.java
+++ src/main/java/com/ruslandev/springbeancreation/car/beans/MyBean.java
@@ -1,4 +1,21 @@
 package com.ruslandev.springbeancreation.car.beans;
 
 public class MyBean {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private void showMessage() {
+        System.out.println(message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
